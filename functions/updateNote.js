6cae2bc86d1fc959ecb1f24e473e46c584e7a20d
mani-barathi <manibarathi.s@gmail.com>
2021-02-28 14:26:07 +0530

    update note function

---
+++ functions/updateNote.js
@@ -0,0 +1,31 @@
+const faunadb = require('faunadb')
+
+const q = faunadb.query
+const client = new faunadb.Client({
+    secret: process.env.FAUNA_SECRET_KEY
+})
+
+
+exports.handler = async (event) => {
+    if (event.httpMethod != 'POST')
+        return { statusCode: 401, body: JSON.stringify({ message: "Un Authorized request! ", report: false }) }
+
+    const { id, title, text } = JSON.parse(event.body)
+    const updateData = {}
+    if (title) updateData.title = title
+    if (text) updateData.text = text
+
+    console.log(updateData)
+
+    try {
+        const { data, ref, ts } = await client.query(q.Update(q.Ref(q.Collection('notes'), id), { data: updateData }))
+        console.log(data)
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ id: ref.id, ts, data, report: true })
+        }
+    } catch (error) {
+        console.log(error)
+        return { statusCode: 500, body: JSON.stringify({ message: "Somthing went Wrong", report: false }) }
+    }
+}
