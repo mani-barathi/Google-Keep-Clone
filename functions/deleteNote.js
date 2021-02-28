5a85f7988c42cc9f9cfdc6583276af329d8fbd72
mani-barathi <manibarathi.s@gmail.com>
2021-02-28 13:09:49 +0530

    delete note function

---
+++ functions/deleteNote.js
@@ -0,0 +1,22 @@
+const faunadb = require('faunadb')
+
+const q = faunadb.query
+const client = new faunadb.Client({
+    secret: process.env.FAUNA_SECRET_KEY
+})
+
+
+exports.handler = async (event) => {
+    if (event.httpMethod != 'POST')
+        return { statusCode: 401, body: JSON.stringify({ message: "Un Authorized request! ", report: false }) }
+
+    const { id } = JSON.parse(event.body)
+
+    try {
+        const response = await client.query(q.Delete(q.Ref(q.Collection('notes'), id)))
+        return { statusCode: 200, body: JSON.stringify({ message: "Note Deleted", report: true }) }
+    } catch (error) {
+        console.log(error)
+        return { statusCode: 500, body: JSON.stringify({ message: "Somthing went Wrong", report: false }) }
+    }
+}
